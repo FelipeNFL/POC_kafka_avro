f0e0ff838e370750b9c7f0a8fdd2d83b0eca5cb5
FelipeNFL <felipe.nunes@fdex.com.br>
2018-06-13 11:07:05 -0300

    improvements log consumer

--- consumer.py
+++ consumer.py
@@ -13,19 +13,20 @@ def process_v1(value):
     word = value['word']
     print('Palavra: {}'.format(word))
     print('Essa palavra tem {} caracter(es)'.format(len(word)))
+    print('Text to process v1')
 
 def process_v2(value):
     phrase = value['phrase']
     print('Frase: {}'.format(phrase))
     print('Essa frase tem {} caracter(es)'.format(len(phrase)))
     print('Essa frase tem {} palavra(s)'.format(len(phrase.split())))
+    print('Text to process v2')    
 
 def desserialize_avro_message(message):
 
     with DataFileReader(message, DatumReader()) as reader:
 
         if not is_schema_valid(reader):
-            logging.warn('A message was received with wrong schema')
             return None
 
         for message in reader:
@@ -48,7 +49,11 @@ def is_schema_valid(reader):
     schema_text = reader.meta['avro.schema'].decode()
     schema = json.loads(schema_text)
 
-    return schema['name'] == MY_TOPIC
+    if schema['name'] != MY_TOPIC:
+        logging.warn('A message was received with wrong schema ({})'.format(schema['name']))
+        return False
+
+    return True
 
 schemas = get_schemas()
 processors = {'v1': process_v1, 'v2': process_v2}
